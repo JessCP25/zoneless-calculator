64465cb725313d2af228a10709210e09d04fc649
JessCP25 <jpayanoc25@gmail.com>
2026-07-23 22:40:28 -0500

    feat: viewEncapsulation

--- src/app/calculator/components/calculator-button/calculator-button.ts
+++ src/app/calculator/components/calculator-button/calculator-button.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, HostBinding, input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, input, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -8,7 +8,8 @@ import { ChangeDetectionStrategy, Component, HostBinding, input, OnInit } from '
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400'
-  }
+  },
+  // encapsulation: ViewEncapsulation.None,
 })
 export class CalculatorButton {
   public isCommand = input(false, {
@@ -16,7 +17,12 @@ export class CalculatorButton {
       typeof value === 'string' ? value === '' : value
   });
 
-  @HostBinding('class.is-command') get commandStyle(){
-    return this.isCommand();
+  public isDoubleSize = input(false, {
+    transform: (value: string|boolean) =>
+      typeof value === 'string' ? value === '' : value
+  })
+
+  @HostBinding('class.w-2/4') get commandStyle(){
+    return this.isDoubleSize();
   }
 }
--- src/app/calculator/components/calculator/calculator.ts
+++ src/app/calculator/components/calculator/calculator.ts
@@ -6,5 +6,12 @@ import { CalculatorButton } from "../calculator-button/calculator-button";
   imports: [CalculatorButton],
   templateUrl: './calculator.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  // styles: `
+  //   @reference "../../../../styles.css";
+
+  //   .is-command{
+  //     @apply bg-indigo-700/10 text-2xl;
+  //   }
+  // `
 })
 export class Calculator {}
